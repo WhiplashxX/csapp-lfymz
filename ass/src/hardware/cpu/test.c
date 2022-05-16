8d90f729e7573063642978d89d46625dc82c076d
goxx <1551371447@qq.com>
2022-05-16 10:46:09 +0800

    test big/little endian

---
+++ ass/src/hardware/cpu/test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    union test
+    {
+        unsigned long a;
+        unsigned char b[8];
+    }t;
+    t.a = 0x12345678;
+    printf("%x,%x,%x,%x,%x,%x,%x,%x,",t.b[0],t.b[1],t.b[2],t.b[3],t.b[4],t.b[5],t.b[6],t.b[7]);
+}
